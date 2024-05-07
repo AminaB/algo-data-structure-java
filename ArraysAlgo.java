45e94acacf22fc2a8e2284b2b8a57ac888a40389
AminaB <aminatou.barry97@gmail.com>
2024-05-07 11:51:06 +0200

    reverse copy  array

--- ArraysAlgo.java
+++ ArraysAlgo.java
@@ -44,9 +44,16 @@ public class ArraysAlgo {
         }
         return result;
     }
+    public static  int[] reverseCopyArray(int [] array){
+        int[] result=new int[array.length];
+        for(int i=array.length-1, j=0 ; i>=0; i-- , j++){
+            result[j]=array[i];
+        }
+        return result;
+    }
     public static void main(String[] args) {
         int[] arrayTest = {10,3, 5, 8, 9, 1};
-        System.out.println(Arrays.toString(deleteElement(arrayTest,  2)));
+        System.out.println(Arrays.toString(reverseCopyArray(arrayTest)));
 
     }
 }
