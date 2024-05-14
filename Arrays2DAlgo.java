4684b199294b96196a2c0c349456ec450aac6457
AminaB <aminatou.barry97@gmail.com>
2024-05-14 10:45:00 +0200

    multiply matrix

--- Arrays2DAlgo.java
+++ Arrays2DAlgo.java
@@ -11,17 +11,31 @@ public class Arrays2DAlgo {
         }
         return sum;
     }
+    public static int[][] multiplyMatrix(int[][] m1,int[][]m2){
+        int [][] mult= new int[m1.length][m2[0].length];
+        // matrix have same size and not null
+        for(int i=0;i< m1.length; i++){
+            for(int j=0;j< m1[i].length; j++){
+                for(int k=0;k< m2[0].length; k++) {
+                    mult[i][j] += m1[i][k] * m2[k][j];
+                }
+
+            }
+
+        }
+        return mult;
+    }
     public static void main(String[] args) {
         int[][] arrayTest = {
-                {10, 3, 5, 8},
-                {5, 2, 5, 5},
-                {5, 2, 5, 5},
+                {1, 3, 5},
+                {5, 2, 5},
+                {2, 2, 1},
         };
         int[][] arrayTest2 = {
-                {10, 3, 5, 8},
-                {5, 2, 5, 5},
-                {5, 2, 5, 5},
+                {1, 1, 2},
+                {2, 1, 0},
+                {2, 2, 1},
         };
-        System.out.println(Arrays.deepToString(addingMatrix(arrayTest, arrayTest2)));
+        System.out.println(Arrays.deepToString(multiplyMatrix(arrayTest, arrayTest2)));
     }
 }
