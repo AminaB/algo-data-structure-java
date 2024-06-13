a47ddbfed45724a7ed7d25ba7850f847802640f5
AminaB <aminatou.barry97@gmail.com>
2024-06-13 11:42:37 +0200

    lambda expressions as parameters or object

--- cours/LambdaDemo.java
+++ cours/LambdaDemo.java
@@ -8,20 +8,26 @@ interface MyLambda2{
     public void display();
 }
 public class LambdaDemo {
-
-    public void method1(){
+    public static void callLambda(MyLambda2 ml2){
+        ml2.display();
+    }
+    public static void method1(){
         MyLambda2 m2= ()-> {
             int count=0;
             System.out.println("hello");
             System.out.println("Bye "+count++);
 
         };
-        m2.display();;
+        m2.display();
+        callLambda(()-> {System.out.println("hello");});
+        callLambda(m2);
     }
 
     public static void main(String[] args) {
         MyLambda m = (x,y) -> x+y;
         System.out.println(m.add(1,8));
+
+        method1();
 
     }
 }
